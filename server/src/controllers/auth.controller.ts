1838544d758d6c20d49c6ec68fea1c766cf683b3
Md Sazzadul Islam <md.sazzadul.islam15@gmail.com>
2026-07-11 16:23:02 +0600

    feat: implement user registration and login controllers

---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import { loginUser, registerUser } from "../services/auth.service";
+
+export const register = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await registerUser(req.body);
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await loginUser(req.body);
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
