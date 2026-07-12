90cf5fb4a29f363678495c5e33f385dfc880d499
Md Sazzadul Islam <md.sazzadul.islam15@gmail.com>
2026-07-12 12:06:23 +0600

    feat: restructure App component to implement routing with React Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,36 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/auth/LoginPage";
+import RegisterPage from "./pages/auth/RegisterPage";
+import DashboardPage from "./pages/dashboard/DashboardPage";
+import NotFoundPage from "./pages/errors/NotFoundPage";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import RoleRoute from "./routes/RoleRoute";
+
+export default function App() {
   return (
-    <div>
-      <h1>KBD</h1>
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/admin"
+        element={
+          <RoleRoute allowedRoles={["admin"]}>
+            <div>Admin Page</div>
+          </RoleRoute>
+        }
+      />
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
-
-export default App;
