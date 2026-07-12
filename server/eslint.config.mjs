ba626662663ce8ed498134535735375f826fbe84
Md Sazzadul Islam <md.sazzadul.islam15@gmail.com>
2026-07-12 11:25:03 +0600

    feat: refactor ESLint configuration to use TypeScript ESLint setup

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -2,16 +2,24 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default [
-  js.configs.recommended,
+export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
+  files: ["**/*.ts"],
 
-  ...tseslint.configs.recommended,
-
-  {
-    files: ["**/*.ts"],
-
-    languageOptions: {
-      globals: globals.node,
-    },
+  languageOptions: {
+    globals: globals.node,
   },
-];
+
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+    "no-console": "warn",
+    "prefer-const": "error",
+    "no-var": "error",
+  },
+});
