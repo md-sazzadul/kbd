78054c96227596121241d21663842a10fcea72a9
Md Sazzadul Islam <md.sazzadul.islam15@gmail.com>
2026-07-14 13:21:00 +0600

    feat: add request and response interceptors to axios instance

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -10,4 +10,39 @@ const api = axios.create({
   },
 });
 
+/*
+|--------------------------------------------------------------------------
+| Request Interceptor
+|--------------------------------------------------------------------------
+|
+| We'll automatically attach the JWT token here in Step 10.
+|
+*/
+
+api.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+/*
+|--------------------------------------------------------------------------
+| Response Interceptor
+|--------------------------------------------------------------------------
+|
+| Later we'll:
+|
+| - Handle 401 Unauthorized
+| - Refresh expired tokens
+| - Redirect to login
+|
+*/
+
+api.interceptors.response.use(
+  (response) => response,
+
+  (error) => Promise.reject(error),
+);
+
 export default api;
