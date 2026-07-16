ba42e4ae663e25d82e0e75986289072b1abfaa87
Md Sazzadul Islam <md.sazzadul.islam15@gmail.com>
2026-07-17 01:54:26 +0600

    fix: correct import path for AuthProvider in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthProvider";
 import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
