48c9f71a2a212cd7bc9ed89472f85449ca3dc2ce
Md Sazzadul Islam <md.sazzadul.islam15@gmail.com>
2026-07-16 17:24:02 +0600

    fix: enhance ProtectedRoute to handle loading state and user authentication

--- client/src/routes/ProtectedRoute.tsx
+++ client/src/routes/ProtectedRoute.tsx
@@ -1,16 +1,54 @@
-import type { ReactNode } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
+
+import Spinner from "../components/common/Spinner";
+import { useAuth } from "../hooks/useAuth";
 
 interface ProtectedRouteProps {
-  children: ReactNode;
+  children: React.ReactNode;
 }
 
 export default function ProtectedRoute({ children }: ProtectedRouteProps) {
-  const isAuthenticated = true;
+  const { loading, isAuthenticated } = useAuth();
+
+  const location = useLocation();
+
+  /*
+  |--------------------------------------------------------------------------
+  | Still Restoring Session
+  |--------------------------------------------------------------------------
+  */
+
+  if (loading) {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  /*
+  |--------------------------------------------------------------------------
+  | User Not Logged In
+  |--------------------------------------------------------------------------
+  */
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
+    return (
+      <Navigate
+        to="/login"
+        replace
+        state={{
+          from: location,
+        }}
+      />
+    );
   }
+
+  /*
+  |--------------------------------------------------------------------------
+  | Logged In
+  |--------------------------------------------------------------------------
+  */
 
   return children;
 }
