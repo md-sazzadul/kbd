14b30e4f0e8af092050e955e6b4b571a2e2d18c5
Md Sazzadul Islam <md.sazzadul.islam15@gmail.com>
2026-07-16 17:44:08 +0600

    feat: create ProfilePage component with DashboardLayout

---
+++ client/src/pages/profile/ProfilePage.tsx
@@ -0,0 +1,9 @@
+import DashboardLayout from "../../components/layout/DashboardLayout";
+
+export default function ProfilePage() {
+  return (
+    <DashboardLayout>
+      <h1 className="text-4xl font-bold text-white">Profile</h1>
+    </DashboardLayout>
+  );
+}
