9815e42f95d9b9f784841e4c57e89c4e35b5a47b
Md Sazzadul Islam <md.sazzadul.islam15@gmail.com>
2026-07-16 11:28:00 +0600

    refactor: simplify login function syntax in auth.api.ts

--- client/src/api/auth.api.ts
+++ client/src/api/auth.api.ts
@@ -1,9 +1,8 @@
 import { api } from ".";
 import type { AuthResponse, LoginRequest, RegisterRequest } from "../types";
 
-export const login = (data: LoginRequest) => {
-  return api.post<AuthResponse>("/auth/login", data);
-};
+export const login = (data: LoginRequest) =>
+  api.post<AuthResponse>("/auth/login", data);
 
 export const register = (data: RegisterRequest) => {
   return api.post<AuthResponse>("/auth/register", data);
