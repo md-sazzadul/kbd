7f9920603aee569651546eca512d1b0d8ff41e4a
Md Sazzadul Islam <md.sazzadul.islam15@gmail.com>
2026-07-16 17:14:46 +0600

    feat: add request interceptor to attach JWT token for authorization

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getToken } from "../utils/token";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -21,6 +22,12 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
+    const token = getToken();
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
     return config;
   },
   (error) => Promise.reject(error),
